2aa842d4a865db7a624a3f5783da9e7629c553bd
Pong Loong Yeat <pongloongyeat@gmail.com>
2023-12-13 22:32:47 +0800

    chore: update template

--- 2023/template/src/part_1.rs
+++ 2023/template/src/part_1.rs
@@ -1,3 +1,16 @@
 pub fn process(input: &str) -> u32 {
     todo!()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::process;
+
+    #[test]
+    fn can_process_input() {
+        let input = todo!();
+        let result = process(input);
+
+        assert_eq!(result, todo!());
+    }
+}
